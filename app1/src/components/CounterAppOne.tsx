881de6f196b37e391a56c2771674acc43687e788
Cory House <housecor@gmail.com>
2022-12-31 18:20:12 -0600

    Accept props

--- app1/src/components/CounterAppOne.tsx
+++ app1/src/components/CounterAppOne.tsx
@@ -2,14 +2,19 @@ import { Text, Button, Flex } from "@chakra-ui/react";
 import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
-const Counter = () => {
+type Props = {
+  parentCount?: number;
+};
+
+export default function Counter(props: Props) {
   const [count, setCount] = useState(0);
 
   const location = useLocation();
 
   return (
     <Flex color="#000" gap="1rem" direction="column">
-      <Text>Click count : {count} </Text>
+      <Text>Click count: {count} </Text>
+      <Text>Parent count: {props.parentCount} </Text>
       <Button onClick={() => setCount(count + 1)}>Click me</Button>
       {location.pathname !== "/" && (
         <Button as={Link} to="/">
@@ -18,6 +23,4 @@ const Counter = () => {
       )}
     </Flex>
   );
-};
-
-export default Counter;
+}
